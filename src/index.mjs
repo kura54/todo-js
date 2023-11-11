83d1cab448a6cda65538fd13a7e98ba953731717
kura <91504106+kura54@users.noreply.github.com>
2023-11-11 08:12:06 +0000

    Update index.html and index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,23 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  alert();
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //divを生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグを生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
